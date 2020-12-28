bf389cb52cbd93e1f16855632764e3e3a0d85afa
shemishtamesh <62944862+shemishtamesh@users.noreply.github.com>
2020-12-29 00:20:08 +0200

    Update waifu bot.py

--- waifu bot.py
+++ waifu bot.py
@@ -1,16 +1,13 @@
+#imports
 import pyppeteer as pp
 from pyppeteer import launch
 import appdirs
 import os
 import asyncio
 import time
-import win32api
-x, y = win32api.GetCursorPos()
 
 os.environ['PYPPETEER_HOME'] = appdirs.user_data_dir("pyppeteer")
 
-x = []
-y = []
 print("pick a row (1-4)")
 x.append(int(input()) - 1)
 print("pick a position (1-4)")
@@ -29,37 +26,28 @@ async def main():
         autoClose=False
     )
     page = await browser.newPage()
-    await page.setViewport({'width': 800, 'height': 900})
     await page.goto('https://waifulabs.com/')
     await page.evaluate("""{window.scrollBy(0, document.body.scrollHeight);}""")
-    await page.mouse.click(700, 700)
-    await page.mouse.click(460, 250)
-    time.sleep(4)
-    x_poses = [140+180*pos for pos in x]
-    y_poses = [180+170*pos for pos in y]
+    await page.mouse.hover(find_start_btn())
+    await page.mouse.click()
+    time.sleep(5)
+    positions = []
+    for x_pos, y_pos in x, y:
+        positions.append(x+4*y)
     for i in range(4):
-        await page.mouse.click(x_poses[i],y_poses[i])
+        girls = []
+        await page.mouse.hover(girls[i])
+        actual_click(page)
         time.sleep(5)
-    
-
-
 	
-async def FindAllGirls(page):
-	list = page.querySelectorAll(".girl")
-	return list
-async def FindStartBtn(page)
+async def find_all_girls(page):
+	return page.querySelectorAll(".girl")
+
+async def find_start_btn(page):
 	return page.querySelector(".button.block.blue")
 
-
-
-    if "start" == input():
-        while True:
-            print(win32api.GetCursorPos())
-            time.sleep(2)
-    else:
-        print("Stopped.")
-
-        
+async def actual_click(page):
+    page.mouse.down()
+    page.mouse.up()
 
 asyncio.get_event_loop().run_until_complete(main())
-
