f1fabb9c17771c6a9aed972ac29a15914560cc6b
ranshaa05 <ranshaa05@users.noreply.github.com>
2021-02-18 18:53:17 +0200

    now fully works! id like to thank my mom, my friends and co
    close button still gets stuck every once in a while :(

--- Waifu Bot.py
+++ Waifu Bot.py
@@ -13,38 +13,59 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 
 os.environ['PYPPETEER_HOME'] = appdirs.user_data_dir("pyppeteer")
 
-secret = "ODA5MDQ2NzY2MzEzOTMwNzYy.YCPZhA.PItlTYokiM82pW6gJKSr1oz-yAY"
+secret = ""
 
 client = commands.Bot(command_prefix = "$", Intents = discord.Intents().all())
 @client.command()
 
 
 async def waifu(ctx, *, start):
-    await ctx.channel.send(f"Hello! I am WaifuBot! I make waifus using waifulabs.com. let's start making your waifu!\nStart by telling me the position of your waifu on the following grid:")
-       
-    await ctx.channel.send(f"Syntax for your answer must be 'x, y'. x represents the horizontal position of your waifu and y represents the vertical position.\nThe starting point is at the top left cornet of the grid.\nYour answer:")
+    async def askposclick(page):
+        msg = await client.wait_for("message")
+        while not await check(msg, page):
+            msg = await client.wait_for("message")
+        msg = msg.content
+        if not msg == "keep":
+            x = int(msg[0]) - 1
+            y = int(msg[3]) - 1
+            pos = x + 4 * y
+            time.sleep(1.5)
+            await wait_for_all_girls(page)
+            girls = await find_all_girls(page)
+            await girls[pos].click()
+
+        else:
+            await ((await page.querySelectorAll(".keep-button"))[0]).click()
 
 
-    async def check(msg):
-        retvalue = True
+
+    async def check(msg, page):
         if not (msg.author == ctx.author and msg.channel == ctx.channel):
-            retvalue = False
+            return False
         
         msg = msg.content
 
-        try:
-            if not 0 < int(msg[0]) < 5 and 0 < int(msg[3]) < 5 and msg.find(", ") == 1 and len(msg) == 5:              #makes sure the user input is valid. to do: for some reason it accepts inputs higher or lower than 4 and longer or shorter than 5 chars, but does not accept non-integers
-                await ctx.channel.send("Whoops! Wrong syntax. The correct syntax is 'x, y'.")
+        if msg == "$waifu start":
+            return False
+
+        if msg == "keep" and len(await page.querySelectorAll(".keep-button")) < 1:
+            await ctx.channel.send("You haven't selected an initial waifu yet! Try something like 'x, y'.")
+            return False
+        if msg != "keep":
+            try:
+                if not (msg.find(", ") == 1 and len(msg) == 4):
+                    await ctx.channel.send("Whoops! Wrong syntax. The correct syntax is 'x, y'.")
+                    return False
+                
+                if not (0 < int(msg[0]) < 5 and 0 < int(msg[3]) < 5):              #makes sure the user input is valid.
+                    await ctx.channel.send("Numbers too big or small! Try something between 1 and 4 :)")
+                    return False
+
+            except ValueError:
+                await ctx.channel.send("Whoops! Wrong syntax. The correct syntax is 'x, y'. x and y must be numbers.")
                 return False
 
-        except IndexError:
-            await ctx.channel.send("Whoops! Numbers too big! Try something between 1 and 4 :)")                        # no idea if we even need this
-
-        except ValueError:
-            await ctx.channel.send("Whoops! Wrong syntax. The correct syntax is 'x, y'. x and y must be numbers.")
-            return False
-        
-        return retvalue
+        return True
             
 
 
@@ -58,26 +79,29 @@ async def waifu(ctx, *, start):
         await page.setViewport({'width': 1550, 'height': 1000})
         await page.goto('https://waifulabs.com/')
         await (await find_start_btn(page)).click()
+        
 
         await wait_for_close_button(page)
-        
+
         await (await find_close_button(page)).click()
 
+        await ctx.channel.send(f"Hello! I am WaifuBot! I make waifus using waifulabs.com. let's start making your waifu!\nStart by telling me the position of your waifu on the following grid:")
+        time.sleep(3)
+        await page.screenshot({'path': dir_path + '\grid1.png'}, clip = {"x": 430, "y": 250, "height": 690, "width": 690})
+        await ctx.channel.send(file=discord.File(dir_path + '\grid1.png'))
+        await ctx.channel.send(f"Syntax for your answer must be 'x, y'. x represents the horizontal position of your waifu and y represents the vertical position.\nThe starting point is at the top left cornet of the grid. you can also type 'keep' to continue with your current waifu.\nYour answer:")
         
-        for i in range(0,4):
-            msg = await client.wait_for("message")
-            while not await check(msg):
-                msg = await client.wait_for("message")
-            msg = msg.content
-            x = int(msg[0]) - 1
-            y = int(msg[3]) - 1
-            pos = x + 4 * y
+        for i in range(0,3):
+            await askposclick(page)
 
-            time.sleep(1.5)
-            await wait_for_all_girls(page)
-            girls = await find_all_girls(page)
-            await girls[pos].click()
+            time.sleep(3)
+            await page.screenshot({'path': dir_path + '\grid2.png'}, clip = {"x": 150, "y": 255, "height": 690, "width": 1250})
+            await ctx.channel.send("Okay! lets continue. Here's another grid for you to choose from:")
+            await ctx.channel.send(file=discord.File(dir_path + '\grid2.png'))
             
+            
+        await askposclick(page)
+
         time.sleep(2)
         await (await page.querySelector(".my-girl-image")).screenshot({'path': dir_path + '\end_result.png'})             #saves screenshot of result page
 
@@ -92,21 +116,19 @@ async def waifu(ctx, *, start):
 async def find_all_girls(page):
     return await page.querySelectorAll(".girl")
 
-
 async def find_start_btn(page):
     return await page.querySelector('.button.block.blue')
-
 
 async def find_close_button(page):
     return await page.querySelector('.sc-bxivhb.eTpeTG.sc-bdVaJa.cYQqRL')
 
 async def wait_for_close_button(page):
     while not await find_close_button(page):
-        print(not await find_close_button(page))
+        time.sleep(0.01)
 
 async def wait_for_all_girls(page):
         while len(await find_all_girls(page)) < 16:
-            pass
+            time.sleep(0.01)
         
 
 client.run(secret)
