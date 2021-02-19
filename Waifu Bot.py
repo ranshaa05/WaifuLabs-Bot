ab238e1313ce2910efe21a0a2d8cfc5aef388446
ranshaa05 <ranshaa05@users.noreply.github.com>
2021-02-19 13:39:46 +0200

    added stop command

--- Waifu Bot.py
+++ Waifu Bot.py
@@ -65,6 +65,11 @@ async def waifu(ctx, *, start):
                 await ctx.channel.send("Whoops! Wrong syntax. The correct syntax is 'x, y'. x and y must be numbers.")
                 return False
 
+                if msg == "stop":
+                    await browser.close()
+                    ctx.channel.send("Okay! See you next time :)")
+
+
         return True
             
 
