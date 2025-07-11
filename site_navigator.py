8826107ecddc6f5cec2fecda9abf1d337e802fdb
Ran Shaashua <ranshaa05@gmail.com>
2025-07-11 11:10:57 +0300

    fixed asyncio double event loop

--- site_navigator.py
+++ site_navigator.py
@@ -1,86 +1,74 @@
-import asyncio
-import functools
 from pyppeteer import launch
 from random import randint
 from time import sleep
 
-def _page_stack(coroutine):
-    """a decorator that takes control of the browser and returns it to the page of the previous user"""
-
-    @functools.wraps(coroutine)
-    async def wrapper(self, *args, **kwargs):
-        previous_page = PageNavigator.active_page
-        await self.page.bringToFront()
-        return_value = await coroutine(self, *args, **kwargs)
-        if previous_page:
-            await previous_page.bringToFront()
-        PageNavigator.active_page = previous_page
-        return return_value
-
-    return wrapper
+UNDO_BUTTON_SELECTOR = ".sc-bdvvtL:nth-child(1)"
+KEEP_BUTTON_SELECTOR = ".sc-bdvvtL:nth-child(2)"
+WAIFU_GRID_SELECTOR = ".waifu-grid > div"
+LOADING_SELECTOR = ".loading-callout"
+FINAL_IMAGE_LOADING_SELECTOR = ".waifu-preview-loading"
+MAX_GRID_CHOICES = 15
+REFRESH_BUTTON_INDEX = 15  # The 16th element is the refresh button
 
 
 class PageNavigator:
-    browser = asyncio.get_event_loop().run_until_complete(
-        launch(headless=True, autoClose=True)
-    )
-    active_page = None
+    browser = None
 
     def __init__(self):
         self.page = None
         self.timed_out = False
 
     @staticmethod
+    async def _initialize_browser():
+        """Initializes the pyppeteer browser instance if it hasn't been already."""
+        if PageNavigator.browser is None or not PageNavigator.browser.isConnected():
+            PageNavigator.browser = await launch(headless=True, autoClose=True)
+
+    @staticmethod
     async def create_navi():
+        await PageNavigator._initialize_browser()
         navi = PageNavigator()
         navi.page = await PageNavigator.browser.newPage()
         await navi.page.setViewport({"width": 1200, "height": 630})
         await navi.page.goto("https://waifulabs.com/generate")
         return navi
 
-    @_page_stack
     async def click_by_index(self, index):
         girls = await self.find_all_girls()
         await girls[index - 1].click()
 
-    @_page_stack
     async def exit(self):
-        await self.page.close()
+        if self.page and not self.page.isClosed():
+            await self.page.close()
 
-    @_page_stack
     async def undo(self):
-        await self.page.click(".sc-bdvvtL:nth-child(1)")
+        await self.page.click(UNDO_BUTTON_SELECTOR)
 
-    @_page_stack
     async def keep(self):
-        await self.page.click(".sc-bdvvtL:nth-child(2)")
+        await self.page.click(KEEP_BUTTON_SELECTOR)
 
-    @_page_stack
     async def rand(self):
-        random_index = randint(1, 15)
-        await self.page.click(f".waifu-grid > div:nth-child({random_index})")
+        random_index = randint(1, MAX_GRID_CHOICES)
+        await self.page.click(f"{WAIFU_GRID_SELECTOR}:nth-child({random_index})")
 
-    @_page_stack
     async def refresh(self):
-        await (await self.find_all_girls())[15].click()
+        await (await self.find_all_girls())[REFRESH_BUTTON_INDEX].click()
 
-    @_page_stack
     async def wait_for_not_load_screen(self):
-        while await self.page.querySelector(".loading-callout"):
+        while await self.page.querySelector(LOADING_SELECTOR):
             sleep(0.01)
 
-    @_page_stack
     async def wait_for_final_image(self):
-        while await self.page.querySelector(".waifu-preview-loading"):
+        while await self.page.querySelector(FINAL_IMAGE_LOADING_SELECTOR):
             sleep(0.01)
 
-    @_page_stack
     async def screenshot(self, selector):
-        screenshot_bytes = await (await self.page.querySelector(selector)).screenshot()
+        element = await self.page.querySelector(selector)
+        screenshot_bytes = await element.screenshot()
         return screenshot_bytes
 
     async def find_all_girls(self):
-        return await self.page.querySelectorAll(".waifu-grid > div")
+        return await self.page.querySelectorAll(WAIFU_GRID_SELECTOR)
 
     async def page_timeout(self):
         # this is to differentiate between a timeout and a user exiting.
